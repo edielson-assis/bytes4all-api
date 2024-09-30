f8f899896669ccf28832276bea7e6e049659daff
Edielson Assis <edielsonnice@hotmail.com>
2024-09-30 00:32:25 -0300

    test: implemented tests for person entity and person VO

---
+++ src/test/java/br/com/edielsonassis/bookstore/unittests/mapper/DozerConverterTest.java
@@ -0,0 +1,118 @@
+package br.com.edielsonassis.bookstore.unittests.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.edielsonassis.bookstore.data.vo.v1.AddressVo;
+import br.com.edielsonassis.bookstore.data.vo.v1.PersonVo;
+import br.com.edielsonassis.bookstore.mapper.Mapper;
+import br.com.edielsonassis.bookstore.model.Address;
+import br.com.edielsonassis.bookstore.model.Person;
+
+public class DozerConverterTest {
+    
+    MockPerson inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockPerson();
+    }
+
+    @Test
+    public void parseEntityToVOTest() {
+        PersonVo output = Mapper.parseObject(inputObject.mockEntity(), PersonVo.class);
+        assertEquals(Long.valueOf(0L), output.getPersonId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals(addressVo(0).toString(), output.getAddress().toString());
+        assertEquals("MALE", output.getGender().name());
+    }
+
+    @Test
+    public void parseEntityListToVOListTest() {
+        List<PersonVo> outputList = Mapper.parseListObjects(inputObject.mockEntityList(), PersonVo.class);
+        PersonVo outputZero = outputList.get(0);
+        
+        assertEquals(Long.valueOf(0L), outputZero.getPersonId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals(addressVo(0).toString(), outputZero.getAddress().toString());
+        assertEquals("MALE", outputZero.getGender().name());
+        
+        PersonVo outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getPersonId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals(addressVo(7).toString(), outputSeven.getAddress().toString());
+        assertEquals("FEMALE", outputSeven.getGender().name());
+        
+        PersonVo outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getPersonId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals(addressVo(12).toString(), outputTwelve.getAddress().toString());
+        assertEquals("MALE", outputTwelve.getGender().name());
+    }
+
+    @Test
+    public void parseVOToEntityTest() {
+        Person output = Mapper.parseObject(inputObject.mockVO(), Person.class);
+        assertEquals(Long.valueOf(0L), output.getPersonId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals(address(0).toString(), output.getAddress().toString());
+        assertEquals("MALE", output.getGender().name());
+    }
+
+    @Test
+    public void parserVOListToEntityListTest() {
+        List<Person> outputList = Mapper.parseListObjects(inputObject.mockVOList(), Person.class);
+        Person outputZero = outputList.get(0);
+        
+        assertEquals(Long.valueOf(0L), outputZero.getPersonId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals(address(0).toString(), outputZero.getAddress().toString());
+        assertEquals("MALE", outputZero.getGender().name());
+        
+        Person outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getPersonId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals(address(7).toString(), outputSeven.getAddress().toString());
+        assertEquals("FEMALE", outputSeven.getGender().name());
+        
+        Person outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getPersonId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals(address(12).toString(), outputTwelve.getAddress().toString());
+        assertEquals("MALE", outputTwelve.getGender().name());
+    }
+
+    private AddressVo addressVo(Integer number) {
+        AddressVo addres = new AddressVo();
+        addres.setStreet("Street Test" + number);
+        addres.setNeighborhood("Neighborhood Test" + number);
+        addres.setCity("City Test" + number);
+        addres.setState("State Test" + number);
+        return addres;
+    }
+
+    private Address address(Integer number) {
+        Address addres = new Address();
+        addres.setStreet("Street Test" + number);
+        addres.setNeighborhood("Neighborhood Test" + number);
+        addres.setCity("City Test" + number);
+        addres.setState("State Test" + number);
+        return addres;
+    }
+}
---
+++ src/test/java/br/com/edielsonassis/bookstore/unittests/mapper/MockPerson.java
@@ -0,0 +1,75 @@
+package br.com.edielsonassis.bookstore.unittests.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.edielsonassis.bookstore.data.vo.v1.AddressVo;
+import br.com.edielsonassis.bookstore.data.vo.v1.PersonVo;
+import br.com.edielsonassis.bookstore.model.Address;
+import br.com.edielsonassis.bookstore.model.Person;
+import br.com.edielsonassis.bookstore.model.enums.Gender;
+
+public class MockPerson {
+
+    public Person mockEntity() {
+        return mockEntity(0);
+    }
+    
+    public PersonVo mockVO() {
+        return mockVO(0);
+    }
+    
+    public List<Person> mockEntityList() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonVo> mockVOList() {
+        List<PersonVo> persons = new ArrayList<PersonVo>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockVO(i));
+        }
+        return persons;
+    }
+    
+    public Person mockEntity(Integer number) {
+        Person person = new Person();
+        person.setAddress(mockAddress(number));
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? Gender.MALE : Gender.FEMALE);
+        person.setPersonId(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+    public PersonVo mockVO(Integer number) {
+        PersonVo person = new PersonVo();
+        person.setAddress(mockAddressVo(number));
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? Gender.MALE : Gender.FEMALE);
+        person.setPersonId(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+    private AddressVo mockAddressVo(Integer number) {
+        AddressVo addres = new AddressVo();
+        addres.setStreet("Street Test" + number);
+        addres.setNeighborhood("Neighborhood Test" + number);
+        addres.setCity("City Test" + number);
+        addres.setState("State Test" + number);
+        return addres;
+    }
+
+    private Address mockAddress(Integer number) {
+        Address addres = new Address();
+        addres.setStreet("Street Test" + number);
+        addres.setNeighborhood("Neighborhood Test" + number);
+        addres.setCity("City Test" + number);
+        addres.setState("State Test" + number);
+        return addres;
+    }
+}
