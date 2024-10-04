5936209576bbaee1619769a68b0a103cb2ec07de
Edielson Assis <edielsonnice@hotmail.com>
2024-10-04 20:58:40 -0300

    test: implemented integration test for Swagger interface

---
+++ src/test/java/br/com/edielsonassis/bookstore/integrationstests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,32 @@
+package br.com.edielsonassis.bookstore.integrationstests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.edielsonassis.bookstore.config.TestConfig;
+import br.com.edielsonassis.bookstore.integrationstests.AbstractIntegrationTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {"server.port=8888"})
+class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    @DisplayName("JUnit test for Should display UI Swagger page")
+    void testShouldDisplaySwaggerUIPage() {
+        var content = given()
+            .basePath("/swagger-ui/index.html")
+                .port(TestConfig.SERVER_PORT)
+                    .when()
+                        .get()
+                            .then()
+                                .statusCode(200)
+                                    .extract()
+                                        .body()
+                                            .asString();
+
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
