64b235e9ca22e35029002ecb5e4d5f2486ef5831
Edielson Assis <edielsonnice@hotmail.com>
2024-11-18 12:03:04 -0300

    docs: refactored swagger configuration

--- src/main/java/br/com/edielsonassis/bookstore/configs/SpringDocConfig.java
+++ src/main/java/br/com/edielsonassis/bookstore/configs/SpringDocConfig.java
@@ -1,5 +1,6 @@
 package br.com.edielsonassis.bookstore.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +10,13 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SpringDocConfig {
+
+	@Value("${springdoc.server.url}")
+	private String springDocServer;
 
 	@Bean
 	public OpenAPI customOpenAPI() {
@@ -22,6 +27,7 @@ public class SpringDocConfig {
 							.type(SecurityScheme.Type.HTTP)
 							.scheme("bearer")
 							.bearerFormat("JWT")))
+				.addServersItem(new Server().url(springDocServer))
 				.info(new Info()
 					.title("Bytes4All")
 					.version("v1.0.0")
